f7108638eaf9b86c9d6fe2aca75388c6c6f7fd33
mr-sombra <noelopezcamp2003@outlook.com>
2023-11-05 23:51:09 -0700

    db not working

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
+import styles from '@/app/ui/home.module.css'
+import Image from 'next/image';
 
 import Link from 'next/link';
 export default function Page() {
@@ -6,6 +8,8 @@ export default function Page() {
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
+        <AcmeLogo />
+        <div className={styles.shape}/>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -25,6 +29,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image 
+            src="/hero-desktop.png" 
+            alt="Screenshots of the dashboard"
+            width={1000}
+            height={760}
+            className='hidden md:block'
+          />
+          <Image 
+            src="/hero-mobile.png" 
+            alt="Screenshots of the dashboard"
+            width={560}
+            height={620}
+            className='block md:hidden'
+          />
         </div>
       </div>
     </main>
